10c0b73532085398d78d85af9f2ffcb0efb2e858
Valentina Pakhomova <valentina.pakhomova@team.wrike.com>
2020-10-05 19:39:31 +0300

    init

---
+++ imported-styles/web/main.dart
@@ -0,0 +1,17 @@
+// import 'package:angular/angular.dart';
+//
+// import 'assets/main.template.dart' as self;
+//
+// // ignore_for_file: uri_has_not_been_generated
+// // ignore_for_file: argument_type_not_assignable
+// // ignore_for_file: uri_has_not_been_generated
+// // ignore_for_file: unnecessary_cast
+// // ignore_for_file: prefer_const_declarations
+//
+//
+//
+// final InjectorFactory injector = self.injector$Injector as InjectorFactory;
+//
+// void main() {
+//   // runApp<Object>(ng.DemoComponentNgFactory, createInjector: injector);
+// }
